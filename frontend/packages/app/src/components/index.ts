3bec22537353d3947c7b056e99bc0e26dcee2372
chshilpa18 <samitach780@gmail.com>
2026-02-10 12:25:47 -0500

    add EntityPill component

--- frontend/packages/app/src/components/index.ts
+++ frontend/packages/app/src/components/index.ts
@@ -24,3 +24,4 @@ export {
 export { TranscriptionMenu } from './TranscriptionMenu';
 export { TranscriptPanel } from './TranscriptPanel';
 export { SettingsModal } from './SettingsModal';
+export { EntityPill } from "./EntityPill/EntityPill";
