e700d82a4ceaee00bb45a8643b8fdfdb721b10fa
Cameron Target <camerontarget@gmail.com>
2026-08-29 08:50:12 -0400

    added dna-docs
    Signed-off-by: Cameron Target <camerontarget@gmail.com>

---
+++ dna-docs/src/theme/MDXComponents.tsx
@@ -0,0 +1,10 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import Figure from '@site/src/components/Figure';
+
+// Components listed here are available in every .mdx file without an import.
+// Keep this list short - anything used by only one or two docs is better off
+// imported explicitly in those docs.
+export default {
+  ...MDXComponents,
+  Figure,
+};
